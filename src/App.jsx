dc3c512d56083ebd27800ea7f97c1fcbe3d4c2a2
Ilya Zatcepilov <deadburn1395@gmail.com>
2025-07-15 13:16:08 +0500

    init repo and nav bar ui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Button from '@mui/material/Button'
+import NavBar from './components/NavBar'
+
+export default function App() {
+
+  return (
+    <>
+      <NavBar/>
+      {/* <Button variant='contained'>Hello world</Button> */}
+    </>
+  )
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import AppBar from '@mui/material/AppBar'
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import { ReactComponent as LogoIcon } from './assets/logo.svg'
+
+export default function NavBar() {
+  return ( 
+    <AppBar position="static">
+      <Container>
+        <Toolbar>
+          
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
+}
